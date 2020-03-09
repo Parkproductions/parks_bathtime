af61b761b751cfe4970f52554d30db0f56ebc304
Parkproductions <declan.holt@fitcollege.edu.au>
2020-03-09 15:36:23 +1300

    Add files via upload

--- client.lua
+++ client.lua
@@ -1,10 +1,10 @@
 local blips = {
-	{ name = 'Bath Tub', sprite = 662885764, x=-1812.73, y=-347.04, z=166.51 },
+	{ name = 'Bath Tub', sprite = 662885764, x=-1812.73, y=-374.0, z=166.51 },
 }
  
 Citizen.CreateThread(function()
 	for _, info in pairs(blips) do
-        local blip = N_0x554d9d53f696d002(1664425300, info.x, info.y, info.z)
+        local blip = N_0x554d9d53f696d002(662885764, info.x, info.y, info.z)
         SetBlipSprite(blip, info.sprite, 1)
 		SetBlipScale(blip, 0.2)
 		Citizen.InvokeNative(0x9CB1A1623062F402, blip, info.name)
