dc660cbc62dcb000b248cf1ce3b07218cc48ea22
Parkproductions <declan.holt@fitcollege.edu.au>
2020-03-09 15:24:45 +1300

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,31 @@
+local blips = {
+	{ name = 'Bath Tub', sprite = 662885764, x=-1812.73, y=-347.04, z=166.51 },
+}
+ 
+Citizen.CreateThread(function()
+	for _, info in pairs(blips) do
+        local blip = N_0x554d9d53f696d002(1664425300, info.x, info.y, info.z)
+        SetBlipSprite(blip, info.sprite, 1)
+		SetBlipScale(blip, 0.2)
+		Citizen.InvokeNative(0x9CB1A1623062F402, blip, info.name)
+    end  
+end)
+
+--------------------------------------------------------
+
+function whenKeyJustPressed(key)
+    if Citizen.InvokeNative(0x580417101DDB492F, 0, key) then
+        return true
+    else
+        return false
+    end
+end
+
+
+function DisplayHelp(_message, x, y, w, h, enableShadow, col1, col2, col3, a, centre)
+ local str = Citizen.InvokeNative(0xFA925AC00EB830B9, 10, "LITERAL_STRING", _message, Citizen.ResultAsLong())
+    SetTextScale(w, h)
+    SetTextCentre(centre)
+    Citizen.InvokeNative(0xFA233F8FE190514C, str)
+    Citizen.InvokeNative(0xE9990552DEC71600)
+end
