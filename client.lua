d85cb6b6ae749829a94f31ce5e30cc14a9c34f79
Declan Holt <declanholt@parkproductions.net>
2020-03-10 21:04:48 +1300

    added keypress function

--- client.lua
+++ client.lua
@@ -11,3 +11,10 @@ Citizen.CreateThread(function()
     end  
 end)
 
+function whenKeyJustPressed(key)
+    if Citizen.InvokeNative(0x580417101DDB492F, 0, key) then
+        return true
+    else
+        return false
+    end
+end
