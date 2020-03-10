2ee8b723926face63675b0158ed0f24ae119b97c
Declan Holt <declanholt@parkproductions.net>
2020-03-10 22:11:41 +1300

    removed citizen function for blips

--- client.lua
+++ client.lua
@@ -1,15 +1,4 @@
-local blips = {
-	{ name = 'Bath Tub', sprite = 662885764, x=-1812.73, y=-374.0, z=166.51 },
-}
- 
-Citizen.CreateThread(function()
-	for _, info in pairs(blips) do
-        local blip = N_0x554d9d53f696d002(662885764, info.x, info.y, info.z)
-        SetBlipSprite(blip, info.sprite, 1)
-		SetBlipScale(blip, 0.2)
-		Citizen.InvokeNative(0x9CB1A1623062F402, blip, info.name)
-    end  
-end)
+
 
 function whenKeyJustPressed(key)
     if Citizen.InvokeNative(0x580417101DDB492F, 0, key) then
@@ -52,7 +41,7 @@ local function DisplayHelp( _message, x, y, w, h, enableShadow, col1, col2, col3
 end
 
 Citizen.CreateThread(function()
-    
+    while true do
 
         local IsZone, IdZone = IsNearZone( Config.Coords )
 
@@ -61,6 +50,6 @@ Citizen.CreateThread(function()
             
         end
 
-    
+    end
 
 end)
