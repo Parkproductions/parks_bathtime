5aef8ed3f80d7dd99bbbeeebf82198fda1dd0230
Declan Holt <declanholt@parkproductions.net>
2020-03-10 23:09:20 +1300

    removed citizen function for blips

--- client.lua
+++ client.lua
@@ -1,4 +1,15 @@
-
+local blips = {
+	{ name = 'Bath Tub', sprite = 662885764, x=-1812.73, y=-374.0, z=166.51 },
+}
+ 
+Citizen.CreateThread(function()
+	for _, info in pairs(blips) do
+        local blip = N_0x554d9d53f696d002(662885764, info.x, info.y, info.z)
+        SetBlipSprite(blip, info.sprite, 1)
+		SetBlipScale(blip, 0.2)
+		Citizen.InvokeNative(0x9CB1A1623062F402, blip, info.name)
+    end  
+end)
 
 function whenKeyJustPressed(key)
     if Citizen.InvokeNative(0x580417101DDB492F, 0, key) then
@@ -42,14 +53,14 @@ end
 
 Citizen.CreateThread(function()
     while true do
-
+ Wait(10)
         local IsZone, IdZone = IsNearZone( Config.Coords )
-
+ 
         if IsZone then
             DisplayHelp(Config.bathtext, 0.50, 0.95, 0.6, 0.6, true, 255, 255, 255, 255, true, 10000)
-            
+           
         end
-
+ 
     end
-
+ 
 end)
--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.bathtext = 'Press (E) for action with gun shop'
+Config.bathtext = 'Press (E) to take a Bath'
 
 Config.Coords = {
 	vector3(-1812.73,-374.0,166.51),
