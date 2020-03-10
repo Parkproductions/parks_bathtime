091caa42af89e9eec041c5b9aa08b905b466e787
Declan Holt <declanholt@parkproductions.net>
2020-03-10 21:36:01 +1300

    added coords to config

--- config.lua
+++ config.lua
@@ -1,8 +1,8 @@
 Config = {}
 
-Config.bathtext = 'Press (E) for action with gun shop'
+Config.bathtext = 'Press (E) for action with gun shop',
 
 Config.Coords = {
-	vector3(-1812.73,-374.0,166.51)
+	vector3(-1812.73,-374.0,166.51),
 }
 
