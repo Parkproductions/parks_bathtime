16decdb7237f06cc3b9502f53eb70c8f7145b1a9
Declan Holt <declanholt@parkproductions.net>
2020-03-12 10:23:39 +1300

    added lua taskstartscenario

--- client.lua
+++ client.lua
@@ -59,27 +59,14 @@ end
 
 Citizen.CreateThread(function()
     while true do
-    Wait(10)
+ Wait(10)
         local IsZone, IdZone = IsNearZone( Config.Coords )
  
         if IsZone then
-            --[[DisplayHelp(Config.bathtext, 0.50, 0.95, 0.6, 0.6, true, 255, 255, 255, 255, true, 10000)--]]
-            if IsControlJustReleased(0, keys['E']) then
-                TaskStartScenarioInPlace(PlayerPedId(), GetHashKey('WORLD_HUMAN_WASH_FACE_BUCKET_GROUND'), 10000, true, false, false, false)
+            DisplayHelp(Config.bathtext, 0.50, 0.95, 0.6, 0.6, true, 255, 255, 255, 255, true, 10000)
+            TaskStartScenarioInPlace(PlayerPedId(), GetHashKey('WORLD_HUMAN_WASH_FACE_BUCKET_GROUND'), 10000, true, false, false, false)
         end
  
     end
  
 end)
-
-
-
-
-
-
-
-
-
-
-
-
