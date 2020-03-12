f767b989ec7ed78e5afc640dc5a87b8b4337fe1a
Declan Holt <declanholt@parkproductions.net>
2020-03-12 21:29:49 +1300

    added get closest bath

--- client.lua
+++ client.lua
@@ -27,7 +27,7 @@ local function IsNearZone ( location )
 
     local player = PlayerPedId()
     local playerloc = GetEntityCoords(player, 0)
-    local Bath_ID = GetClosestObjectOfType(playerloc.x, playerloc.y, playerloc.z, 1.0, GetHashKey("P_WASHTUB02X"), true) print(Bath_ID)
+    local Bath_ID = GetClosestObjectOfType(playerloc.x, playerloc.y, playerloc.z, 1.0, GetHashKey("P_WASHTUB03X"), true) print(Bath_ID)
 
     for i = 1, #location do
         if #(playerloc - location[i]) < 1.0 then
